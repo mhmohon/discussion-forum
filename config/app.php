1d4c6c05bd8d18fb46a3a0c9dd91e3a94e11176b
mhmohon <mohon.diit33@gmail.com>
2018-02-22 01:03:31 +0600

    add filemanager
    filemanager

--- config/app.php
+++ config/app.php
@@ -161,6 +161,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Unisharp\Laravelfilemanager\LaravelFilemanagerServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
+
     ],
 
     /*
@@ -211,6 +214,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Image' => Intervention\Image\Facades\Image::class,
         
 
     ],
--- resources/views/backend/pages/test.blade.php
+++ resources/views/backend/pages/test.blade.php
@@ -15,7 +15,7 @@
                             
                         </div>
                         <div class="body">
-                            <textarea class="tinymc">
+                            <textarea id="editor">
                                 <h2>WYSIWYG Editor</h2>
                                 
                             </textarea>
@@ -30,63 +30,64 @@
 
 	<script src="{{ asset('js/plugins/tinymce/tinymce.min.js') }}"></script>
   	<script>
-  		tinymce.init({ 
-  			selector:'textarea.tinymc',
-	  		/* theme of the editor */
-			theme: "modern",
-			skin: "lightgray",
-
-			/* width and height of the editor */
-			width: "100%",
+  		tinymce.init({
+		  	selector: '#editor',
+		  	width: "100%",
 			height: 300,
 
 			/* display statusbar */
 			statubar: true,
-			
-			/* plugin */
-			plugins: [
+		  	plugins: [
 				"advlist autolink link image lists charmap print preview hr anchor pagebreak",
 				"searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
 				"save table contextmenu directionality emoticons template paste textcolor"
 			],
+		  	toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image code | print preview media fullpage | forecolor backcolor emoticons",
 
-			/* toolbar */
-			toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons",
-			/* style */
-			style_formats: [
-				{title: "Headers", items: [
-					{title: "Header 1", format: "h1"},
-					{title: "Header 2", format: "h2"},
-					{title: "Header 3", format: "h3"},
-					{title: "Header 4", format: "h4"},
-					{title: "Header 5", format: "h5"},
-					{title: "Header 6", format: "h6"}
-				]},
-				{title: "Inline", items: [
-					{title: "Bold", icon: "bold", format: "bold"},
-					{title: "Italic", icon: "italic", format: "italic"},
-					{title: "Underline", icon: "underline", format: "underline"},
-					{title: "Strikethrough", icon: "strikethrough", format: "strikethrough"},
-					{title: "Superscript", icon: "superscript", format: "superscript"},
-					{title: "Subscript", icon: "subscript", format: "subscript"},
-					{title: "Code", icon: "code", format: "code"}
-				]},
-				{title: "Blocks", items: [
-					{title: "Paragraph", format: "p"},
-					{title: "Blockquote", format: "blockquote"},
-					{title: "Div", format: "div"},
-					{title: "Pre", format: "pre"}
-				]},
-				{title: "Alignment", items: [
-					{title: "Left", icon: "alignleft", format: "alignleft"},
-					{title: "Center", icon: "aligncenter", format: "aligncenter"},
-					{title: "Right", icon: "alignright", format: "alignright"},
-					{title: "Justify", icon: "alignjustify", format: "alignjustify"}
-				]}
-			]
+			// enable title field in the Image dialog
+			image_title: true, 
+			// enable automatic uploads of images represented by blob or data URIs
+			automatic_uploads: false,
+			// URL of our upload handler (for more details check: https://www.tinymce.com/docs/configure/file-image-upload/#images_upload_url)
+			//images_upload_url: 'postAcceptor.php',
+			// here we add custom filepicker only to Image dialog
+			file_picker_types: 'image', 
+			// and here's our custom image picker
+			file_picker_callback: function(cb, value, meta) {
+		    var input = document.createElement('input');
+		    input.setAttribute('type', 'file');
+		    input.setAttribute('name', 'image');
+		    input.setAttribute('accept', 'image/*');
+		    
+		    // Note: In modern browsers input[type="file"] is functional without 
+		    // even adding it to the DOM, but that might not be the case in some older
+		    // or quirky browsers like IE, so you might want to add it to the DOM
+		    // just in case, and visually hide it. And do not forget do remove it
+		    // once you do not need it anymore.
 
+		    input.onchange = function() {
+		      var file = this.files[0];
+		      
+		      var reader = new FileReader();
+		      reader.onload = function () {
+		        // Note: Now we need to register the blob in TinyMCEs image blob
+		        // registry. In the next release this part hopefully won't be
+		        // necessary, as we are looking to handle it internally.
+		        var id = 'blobid' + (new Date()).getTime();
+		        var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+		        var base64 = reader.result.split(',')[1];
+		        var blobInfo = blobCache.create(id, file, base64);
+		        blobCache.add(blobInfo);
 
-  		});
+		        // call the callback and populate the Title field with the file name
+		        cb(blobInfo.blobUri(), { title: file.name });
+		      };
+		      reader.readAsDataURL(file);
+		    };
+		    
+		    input.click();
+		  }
+		});
 
 	  	
   	</script>
